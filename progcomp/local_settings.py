7c05fa967af43903849b71f32ee139298c57a6ae
Doug Patti <douglas.patti@gmail.com>
2012-04-02 00:10:56 -0400

    Replace deprecated database settings

--- progcomp/local_settings.py
+++ progcomp/local_settings.py
@@ -9,13 +9,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '../database/progcomp.db'             # Or path to database file if using sqlite3.
-
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '../database/progcomp.db',            # Or path to database file if using sqlite3.
+        'USER': '',            # Not used with sqlite3.
+        'PASSWORD': '',        # Not used with sqlite3.
+        'HOST': '',            # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',            # Set to empty string for default. Not used with sqlite3.
+    },
+}
 
 MEDIA_ROOT = '../media/'
 USERS_ROOT = MEDIA_ROOT+'users/'
