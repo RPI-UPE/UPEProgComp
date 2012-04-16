c579a148300d4e093e3c29c55335ba38966d60d9
Doug Patti <douglas.patti@gmail.com>
2012-04-16 05:31:08 -0400

    Add trailing slash to url

--- progcomp/submission/urls.py
+++ progcomp/submission/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('progcomp.submission.views',
-	url(r'^download$', 'download', name='download'),
-    url(r'^(\d+)$', 'submit', name='submit'),
-    url(r'^(\d+)/refresh$', 'refresh', name='refresh'),
+    url(r'^download/$', 'download', name='download'),
+    url(r'^(\d+)/$', 'submit', name='submit'),
+    url(r'^(\d+)/refresh/$', 'refresh', name='refresh'),
 )
