e8a4b8802d37868f8d9c18b2720b8a8ecf01c97e
Doug Patti <douglas.patti@gmail.com>
2012-04-22 17:50:50 -0400

    Add competition times to right panel (issue #32)

--- progcomp/context_processors.py
+++ progcomp/context_processors.py
@@ -1,4 +1,12 @@
+import datetime
+
 from django.conf import settings
 
 def profiler(context):
-    return {'PROFILER': settings.PROFILER, 'DEBUG': settings.DEBUG}
+    return {
+        'PROFILER': settings.PROFILER,
+        'DEBUG': settings.DEBUG,
+        'START': settings.START,
+        'END': settings.END,
+        'NOW': datetime.datetime.now(),
+    }
