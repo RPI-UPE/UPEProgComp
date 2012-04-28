14f11d34d18f62dd5a169129fca114e3cbebd51f
Doug Patti <douglas.patti@gmail.com>
2012-04-28 15:38:57 -0400

    Change results page (issue #35)
    - No more /scoreboard/results URI, redirect set up for backwards-compatibility
    - Scoreboard automatically shows extended version for staff
    - Right column removed for staff version
    - Username preserved on hover of name, if necessary

--- progcomp/scoreboard/views.py
+++ progcomp/scoreboard/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 from progcomp.account.models import Profile
 from progcomp.scoreboard.models import Scoreboard
@@ -12,33 +14,31 @@ from progcomp.account.models import is_staff
 
 @login_required
 def scoreboard(request, template='scoreboard/scoreboard.html'):
-    # Check the cache for a response object and return that if set. We're not
-    # using Django's view/template cacher because we cannot invalidate them.
-    sb_cache = cache.get('scoreboard')
-    if sb_cache:
-        return sb_cache
-
-    context = {}
-    ranks, problems = Scoreboard.results()
+    if not request.user.is_staff:
+        # Check the cache for a response object and return that if set. We're not
+        # using Django's view/template cacher because we cannot invalidate them.
+        sb_cache = cache.get('scoreboard')
+        if sb_cache:
+            return sb_cache
+        context = {}
+    else:
+        # If the user is a staff, we show the extended results page
+        # We won't cache this since it won't be checked often and we don't want to
+        # take a chance that the cache was not invalidated when reviewing results
+        context = {'results': True, 'template_fullwidth': True}
 
     # Get information (fname, lname) for each user, map user object over id
+    ranks, problems = Scoreboard.results()
     profiles = Profile.objects.select_related('user').in_bulk(map(lambda x: x[0], ranks))
     context['scoreboard'] = map(lambda y: (profiles[y[0]].user, y[1], y[2], y[3]), ranks)
     context['problems'] = problems
     resp = render_to_response(template, context,
             context_instance=RequestContext(request))
-    cache.set('scoreboard', resp)
+    if not request.user.is_staff:
+        cache.set('scoreboard', resp)
     return resp
 
 @is_staff
-def results(request, template='scoreboard/results.html'):
-    # We won't cache this since it won't be checked often and we don't want to
-    # take a chance that the cache was not invalidated when reviewing results
-    context = {}
-    ranks, problems = Scoreboard.results()
-    profiles = Profile.objects.select_related('user').in_bulk(map(lambda x: x[0], ranks))
-    context['scoreboard'] = map(lambda y: (profiles[y[0]].user, y[1], y[2], y[3]), ranks)
-    context['problems'] = problems
-
-    return render_to_response(template, context,
-            context_instance=RequestContext(request))
+def results(request, template='scoreboard/scoreboard.html'):
+    # Deprecated: keeping this around for backwards compatibility for now
+    return HttpResponseRedirect(reverse('scoreboard'))
