f94c5f167bb890c068a0e46013d2fa36f7cb5217
Doug Patti <douglas.patti@gmail.com>
2012-04-28 21:16:42 -0400

    Better styling for scoreboard statuses (issues #41, #37, #35)

---
+++ progcomp/templatetags/status_text.py
@@ -0,0 +1,16 @@
+from django.template import Library
+
+register = Library()
+
+statuses = {
+	'success': 'Success',
+	'failed': 'Most recent incorrect answer',
+	'invalid': 'Correct answer submitted after competition end',
+	'success first': 'First user to submit a correct solution to this problem',
+}
+
+@register.filter
+def status_text(status):
+	if status in statuses:
+		return statuses[status]
+	return status
